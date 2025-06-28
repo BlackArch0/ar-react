c4f42d1731f956a20b4cbf9e47ba4d852cd323af
BlackArch0 <shaikhar287@gmail.com>
2025-06-28 23:26:33 +0530

    first react push

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,58 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect, useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/card'
 function App() {
-  let [color, changeColor] = useState("bg-red-400")
    
-  return (
-    <>
-        <h1 >Hello This Is AR</h1>
 
-    <h1 className={color} style={(color == "bg-black") ? {color: "white"} : { color: "black" }}>Hello This Is AR</h1>
-    <div className='m-50'>
-       <button onClick={()=>changeColor("bg-green-300")} className='m-5 bg-green-400'>Green</button>
-       <button onClick={()=>changeColor("bg-yellow-300")} className='m-5 bg-yellow-400'>Yellow</button>
-       <button onClick={()=>changeColor("bg-blue-300")} className='m-5 bg-blue-400'>Blue</button>
-       <button onClick={()=>changeColor("bg-orange-300")} className='m-5 bg-orange-400'>Orange</button>
-      <button onClick={()=>changeColor("bg-gray-300")} className='m-5 bg-gray-400'>Gray</button>
-       <button onClick={()=>changeColor("bg-pink-300")} className='m-5 bg-pink-400'>Pink</button>
-       <button onClick={()=>changeColor("bg-purple-300")} className='m-5 bg-purple-400'>Purple</button>
-       <button onClick={()=>changeColor("bg-black")} className='m-5 bg-black text-white'>Black</button>
+ 
+ let [length, setLength] = useState(8)
+ let [nums, setNums] = useState(false)
+ let [chars, setChars] = useState(false)
+ let [passd, setPassd] = useState("")
+
+ let passGen = useCallback(()=>{
+  let pass = ""
+   let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+  if(nums) str += "1234567890"
+  if(chars) str += "!@#$%^&*()_+{}|:"
+  
+  for (let i = 1; i < length; i++) {
+    const char = Math.round(Math.random() * str.length + 1);
+    pass += str.charAt(char)
+  }
+   setPassd(pass)
+ }, [length, nums, chars, setPassd])
+
+useEffect(()=>{
+passGen()
+}, [length, nums, chars, passGen])
+let passref = useRef(null)
+let cptoclip = useCallback(()=>{
+  passref.current?.select()
+  window.navigator.clipboard.writeText(passd)
+}, [passd])
+  return (
+     <div className='flex items-center justify-center'>
+    <div className=' m-50 h-50 h w-55 bg-gray-700'>
+      <input 
+      type="text"
+      value={passd}
+      className='bg-amber-700 '
+      ref={passref}
+      readOnly
+      />
+      <button onClick={cptoclip} className='bg-blue-600 hover:bg-amber-500'>Copy</button>
+      <input type="range" min={6} max={50} value={length} onChange={(e)=>(setLength(e.target.value))}/>
+      <label>Length {length}</label><br />
+      <input checked={nums} type="checkbox" onChange={()=>{setNums((prev)  => !prev)}}/>
+      <label>Numbers</label><br/>
+      <input checked={chars} type="checkbox" onChange={()=>{setChars((prev)  => !prev)}}/>
+      <label>Characters</label>
     </div>
-    </>
+    </div>
   )
 }
 
