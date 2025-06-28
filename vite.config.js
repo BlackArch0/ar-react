e06a228ee3084947134238903f5c9548df9e0c1b
Abdul Rahim Shaikh <93668676+BlackArch0@users.noreply.github.com>
2025-06-28 17:13:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: 
-  [react(),
-  tailwindcss()],
+  base: "/ar-react/", // 👈 must match your GitHub repo name
+  plugins: [react()],
 })
